4a23e2117b11bbc76596cdee0a521d49357f66e5
Olga Yarosh <O_ll_y_a@mail.ru>
2023-08-26 19:29:54 +0300

    создание путей в config

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('habits.urls', namespace='main')),  # подключение путей из приложения main
+    path('user/', include('users.urls', namespace='user')),  # подключение путей из приложения users
 ]
