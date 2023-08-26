353a418f8237ee224ed852a7ae11250c744b6397
Olga Yarosh <O_ll_y_a@mail.ru>
2023-08-27 00:01:30 +0300

    создание прав доступа

--- habits/views/habit.py
+++ habits/views/habit.py
@@ -1,13 +1,27 @@
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from django.shortcuts import get_object_or_404
 
 from habits.models.habit import Habit
+from habits.permissions import IsModerator, IsOwner
 from habits.seriallizers.habit import HabitSerializer
 
 
 class HabitViewSet(ModelViewSet):
     queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+    def get_permissions(self):
+        # Проверка разрешения для разных методов
+        if self.action == 'create':
+            permissions_classes = [IsAuthenticated]
+        elif self.action == 'update' or self.action == 'delete':
+            permissions_classes = [IsModerator | IsOwner]
+        else:
+            permissions_classes = [AllowAny]
+
+        return [permission() for permission in permissions_classes]
 
     def list(self, request, **kwargs):
         queryset = Habit.objects.all()
