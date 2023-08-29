461a6a8b9d539b0d326bbc42ffa14ee980704bc9
Olga Yarosh <O_ll_y_a@mail.ru>
2023-08-29 20:58:11 +0300

    создание пути на создание пользователя

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,15 @@
-from django.conf import settings
-from django.conf.urls.static import static
+
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.urls import path
 
-from django.views.generic import TemplateView
 from users.apps import UsersConfig
-
+from users.views import UserCustomViewSet
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view()),
     path('token/refresh/', TokenObtainPairView.as_view()),
+    path('create/', UserCustomViewSet.as_view({'put': 'create'})),
 
 ]
