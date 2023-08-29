24fa7d95534e98e9ba26d2a40284394c908ed79a
Olga Yarosh <O_ll_y_a@mail.ru>
2023-08-29 20:54:16 +0300

    добавление админки для модели привычек

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models.habit import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'place', 'time_start', 'action', 'is_pleasure', 'is_connect', 'periodic',
+        'days_period', 'reward', 'execution_time', 'is_public')
