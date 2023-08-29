3ec8967b8d2ae3c1964d4e2e26e97ecc3f7fa9c1
Olga Yarosh <O_ll_y_a@mail.ru>
2023-08-29 20:58:30 +0300

    создание пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from users.models import UserCustom
+from users.permissions import IsModerator
+from users.seriallizers import UserCustomSerializer
+
+
+class UserCustomViewSet(ModelViewSet):
+    queryset = UserCustom.objects.all()
+    serializer_class = UserCustomSerializer
+    permission_classes = [IsAuthenticated, IsModerator]
+
+
